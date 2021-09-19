912a17884144b1c9ddaff3c82977977ff4d30acb
lucasandflores <lcs.oliveira93@gmail.com>
2021-09-19 18:37:35 -0300

    finishing tests

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const app = express();
 const port = process.env.PORT
 const rotasEntregas = require('./routes/rotasEntregas')
 const models = require('./db/models')
+const cors = require('cors')
+
+
+app.use(cors())
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 app.use('/v1/entregas', rotasEntregas)
-
-// app.listen(port, () => {
-//    console.log(`server running on port ${port}`) 
-// })
 
 const connect = async () => {try {
     await models.sequelize.authenticate();
